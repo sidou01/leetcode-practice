f3c4b4281f2cbd46e0e4b300fe74ff94443ebe90
sidou01 <sidou010@outlook.com>
2020-03-26 21:05:13 +0000

    fix

--- src/Main.cpp
+++ src/Main.cpp
@@ -1,10 +1,11 @@
 /* Include solutions as header files */
+#include <iostream>
 #include "lt_array.h"
 
 int main()
 {
-    vector<int> arr{1, 7, 3, 6, 5, 6};
+    std::vector<int> arr{1, 7, 3, 6, 5, 6};
     int value = pivotIndex(arr);
-    cout << "Value: " << value << endl;
+    std::cout << "Value: " << value << std::endl;
     return 0;
 }
